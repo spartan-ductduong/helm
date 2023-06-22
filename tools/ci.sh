53f1e2d8184fbbaf8609733f81abbb62c2e4554b
khoitran-c0x12c <khoi.tran@c0x12c.com>
2023-06-22 10:34:11 +0700

    delete [200~redundant~

--- tools/ci.sh
+++ tools/ci.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 # Declare version of index.yaml and Chart.yaml
-INDEX_VERSION="0.1.0"
-CHART_VERSION="0.1.1"
+INDEX_VERSION=0.1.0
+CHART_VERSION=0.1.0
+PROJECT_NAME=chargefuze
 
 if [ ! -f /hosting/index.yml ]; then
   helm repo index hosting
 fi
 
 # Make sure that yq is installed
-if ! [ -x "$(command -v yq)" ]; then
-  apt install yq -y
-  apt install yj -y
+if ! [ "$(command -v yq)" ]; then
+   sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 fi
 
 # Get the value "version" of the chargefuze entry in index.yaml
-yq '.entries.chargefuze[0].version' hosting/index.yaml > INDEX_VERSION
-
+INDEX_VERSION=$(yq '.entries.chargefuze' hosting/index.yaml  | grep "version"| sort -r|head -n 1|yq '.version')
 #get the "version" of chargefuze/Chart.yaml
-yq '.version' chargefuze/Chart.yaml > CHART_VERSION
+CHART_VERSION=$( yq '.version' chargefuze/Chart.yaml ) 
 
-if [ "$CHART_VERSION" > "$INDEX_VERSION" ]; then
+if [ "$CHART_VERSION" \> "$INDEX_VERSION" ]; then
   echo "New version found"
   # Create new version packaged chart in hosting
   helm package chargefuze -d ./hosting

f27c8d9608bb88360e849fcf8ceb555a77b6ca85
Duc Duong <duc.duong@c0x12c.com>
2024-11-17 00:21:07 +0700

    Remove repos

--- tools/sync_repo.sh
+++ tools/sync_repo.sh
@@ -2,14 +2,7 @@
 
 GITHUB_TOKEN=$1
 
-GITHUB_ORGS=(
-  Agora-Livecast
-  Agora-Ursa
-  charge-fuze
-  Liquidity-Financial
-  NuKeyHQ
-  foresight-reporting
-)
+GITHUB_ORGS=()
 
 for org in "${GITHUB_ORGS[@]}"; do
   git remote add "$org" https://"$GITHUB_TOKEN"@github.com/"$org"/infra-helm
